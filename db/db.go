bae998d4ad04a8b5fc5a405088dac13ae75f3294
Abhijit Debnath <102849716+AbhiCodes737@users.noreply.github.com>
2023-07-29 06:26:03 +0530

    Add files via upload

---
+++ db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Db() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb+srv://<username>:<password>@<endpoint>/?retryWrites=true&w=majority")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	return client
+}
